47fab980d1526e19357756c1660d7942dbb6c365
1 <1@303-13>
2020-02-13 17:42:27 +0500

    2 commit

---
+++ VIN_LIB.dll/VIN_LIB.dll/VIN_LIB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace VIN_LIB.dll
+{
+    public class VIN_LIB
+    {
+        public bool CheckVIN(string vin)
+        {
+            string Vinstring;
+            string G;
+            Regex regex = new Regex(@"[0-9|A-H|J-N|P|R-Z]{17}");
+        }
+        public string GetVINCountry(string vin)
+        {
+
+        }
+        public int GetTransportYear (string vin)
+        {
+
+        }
+    }
+}
